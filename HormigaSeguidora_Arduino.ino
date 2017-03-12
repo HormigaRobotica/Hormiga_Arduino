7d8aa71694492efeb1d06b855629d88efd3ab9fe
ANDRBR <andrbr59@gmail.com>
2017-03-12 10:57:44 -0400

    librerias añadidas

---
+++ HormigaSeguidora_Arduino.ino
@@ -0,0 +1,50 @@
+/*
+* Periodo 2017-1
+* U.N.E.F.A. - Núcleo Caracas
+* Carrera: Ingeniería Electrónica
+* Materia: Introducción a la Robótica
+*
+* Hormiga (Seguidora)
+*
+* Este programa se encarga de controlar a una hormiga robótica
+* que recibe la posición de un objeto y se desplaza hacia el mismo una vez recibida esta información
+*/
+
+#include <IRremote.h>
+#include "hormiga.h"
+
+#define PPR 48 // cambiar esto al numero de pasos por revolucion del motor a usar
+#define trigPin 12
+#define echoPin 13
+#define IRreadpin 5
+
+
+SincSteps motores(PPR, 11, 9, 10, 8, PPR, 4, 6, 5, 7);
+
+//Stepper MP(STEPS, 11,9,10,8);
+
+float resultados;
+
+IRrecv irrecv(IRreadpin);
+decode_results results;
+
+void setup() {
+	// Código a ejecutarse solo una vez
+	Serial.begin(9600);
+  irrecv.enableIRIn();
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    resultados = *reinterpret_cast<float*>(&results.value);
+    Serial.print("resultados: ");
+    Serial.println(resultados, 4);
+    irrecv.resume();
+  }
+  
+	motores.desp(1);//
+
+	delay(500);
+}
---
+++ hormiga.cpp
@@ -0,0 +1,16 @@
+/*
+*  Periodo 2017-1
+* U.N.E.F.A. - N�cleo Caracas
+* Carrera: Ingenier�a Electr�nica
+* Materia: Introducci�n a la Rob�tica
+*
+* hormiga.cpp
+*
+* Libreria para el funcionamiento estandar de las "hormigas" roboticas en este proyecto
+*/
+
+#include "hormiga.h"
+
+Hormiga::Hormiga(SincSteps* despl, int colorAzucar, int distObstaculo){
+	
+}
